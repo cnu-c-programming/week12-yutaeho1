f80f780730dc0698a3ab0eb430b26cd7a2ed8f72
yutaeho1 <yut02@naver.com>
2026-05-22 11:44:21 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -6,7 +6,7 @@ int main(int argc, const char* argv[]) {
     
     FILE* fp = fopen(argv[1], "r");
     if (fp == NULL) {
-            perror("file does not exists"); 
+            perror("file dose not exists"); 
             return 1;
         }
 
